8271fc07a2e2846067a04ebe9b8fadad721249b9
Kilativ54 <vitalikechkalov@gmail.com>
2023-09-23 01:58:46 +0200

    Finish

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,35 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { getContacts } from 'redux/selectors';
+import { fetchContacts } from 'redux/operations';
+import { Filter } from './filter/Filter';
+import { ContactList } from './contactList/contactList';
+
+
+
+import { Container } from './App.styled';
+import { ContactForm } from './contactForm/ContactForm';
+
+
+
+export function App() {
+  const dispatch = useDispatch();
+  const { isLoading, error } = useSelector(getContacts);
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
+
+   return (
+    <Container>
+      <h1>Phonebook</h1>
+      <ContactForm />
+      {isLoading && !error && <b>Request in progress...</b>}
+      <h2> Contacts</h2>
+      <Filter  />
+      <ContactList/>
+    </Container>
   );
-};
+}
